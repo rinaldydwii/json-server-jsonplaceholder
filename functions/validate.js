218efadff48344a6012bf1972a3045b39e1e8b37
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-18 22:28:27 +0800

    clean code validate js

--- functions/validate.js
+++ functions/validate.js
@@ -1,72 +1,87 @@
 function validate(body, validator, method = "PATCH", keyMessage = "") {
+    // define default error const
     const error = {
         isEmpty: () => false,
         messages: [],
         body: {}
     }
-    if (method === "PATCH") {
-        const keys = Object.keys(body)
-        const length = keys.length
-        let i = 0
-        do {
-            const key = keys[i]
-            const keyMessages = `${keyMessage}${keyMessage ? "." : ""}${key}`
-            const validatorValue = validator[key]
-            const bodyValue = body[key]
-            const validatorTypeValue = typeof validatorValue
-            const bodyTypeValue = typeof bodyValue
-            if (validatorTypeValue === "object" ) {
-                if (validatorTypeValue !== bodyTypeValue) {
-                    error.messages.push(`${keyMessages} must be an object`)
-                    error.isEmpty = () => true
-                } else {
-                    const errorObject = validate(bodyValue, validatorValue, "POST", keyMessages)
-                    error.isEmpty = () => errorObject.isEmpty()
-                    error.messages.push(errorObject.messages)
+    // define object keys
+    let objectKeys = null
+    if (method === "PATCH")
+        objectKeys = body
+    else if (method === "POST" || method === "PUT") 
+        objectKeys = validator
+    // define keys
+    const keys = Object.keys(objectKeys)
+    // define length keys
+    const length = keys.length
+    let i = 0
+    do {
+        // define key
+        const key = keys[i]
+        // define key messages 
+        const keyMessages = `${keyMessage}${keyMessage ? "." : ""}${key}`
+        // define validator value & validator type value
+        const validatorValue = validator[key]
+        const validatorTypeValue = typeof validatorValue
+        // define body value & body type value
+        const bodyValue = body[key]
+        const bodyTypeValue = typeof bodyValue
+        // check if validator is object
+        if (validatorTypeValue === "object") {
+            // check if body types is not equal with validator
+            if (bodyTypeValue !== validatorTypeValue) {
+                // assing error must be object
+                error.messages.push(`${keyMessages} must be an object`)
+                error.isEmpty = () => true
+            } else {
+                // define error object
+                let errorObject = null
+                if (method === "PATCH") {
+                    // recursive validate for patch method
+                    errorObject = validate(bodyValue, validatorValue, "POST", keyMessages)
+                }
+                else if (method === "POST" || method === "PUT") {
+                    // recursive validate for post or put method
+                    errorObject = validate(bodyValue, validatorValue, method, keyMessages)
+                }
+                // assign error
+                error.isEmpty = () => errorObject.isEmpty()
+                error.messages.push(errorObject.messages)
+                if (method === "PATCH")
+                    // assign body if error isEmpty is false
                     if (!error.isEmpty()) {
                         error.body[key] = errorObject.body
                     }
+            }
+        }
+        // check if validator other than objects
+        else {
+            if (method === "PATCH") {
+                // check if validator is not undefined
+                if (validatorTypeValue !== "undefined") {
+                    // assign body value to body
+                    error.body[key] = bodyValue
                 }
-            }
-            else if (validatorTypeValue !== "undefined") {
-                error.body[key] = bodyValue
-            }
-            i++
-        } while (!error.isEmpty() && i < length)
-        // console.log(error)
-        return error
-    } else if (method === "POST" || method === "PUT") {
-        const keys = Object.keys(validator)
-        const length = keys.length
-        let i = 0
-        do {
-            const key = keys[i]
-            const keyMessages = `${keyMessage}${keyMessage ? "." : ""}${key}`
-            const bodyValue = body[key]
-            const validatorValue = validator[key]
-            const bodyTypeValue = typeof bodyValue
-            const validatorTypeValue = typeof validatorValue
-            if (validatorTypeValue === "object") {
-                if (bodyTypeValue !== validatorTypeValue) {
-                    error.messages.push(`${keyMessages} must be an object`)
+            } else if (method === "POST" || method === "PUT") {
+                // check if body value is undefined
+                if (bodyTypeValue === "undefined") {
+                    // assign error is required
+                    error.messages.push(`${keyMessages} is required`)
                     error.isEmpty = () => true
-                } else {
-                    const errorObject = validate(bodyValue, validatorValue, method, keyMessages)
-                    error.isEmpty = () => errorObject.isEmpty()
-                    error.messages.push(errorObject.messages)
+                }
+                // check if body is not undefined
+                else if (bodyTypeValue !== "undefined") {
+                    // assign body value to body
+                    error.body[key] = bodyValue
                 }
             }
-            else if (bodyTypeValue === "undefined") {
-                error.messages.push(`${keyMessages} is required`)
-                error.isEmpty = () => true
-            }
-            else if (bodyTypeValue !== "undefined") {
-                error.body[key] = bodyValue
-            }
-            i++
-        } while (!error.isEmpty() && i < length)
-        return error
-    }
+        }
+        // increment
+        i++
+    } while (!error.isEmpty() && i < length) // stop is isEmpty is true and i less than length
+    // return error
+    return error
 }
 
 module.exports = validate
