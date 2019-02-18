f6c9326748cfbd03a8c634c2ee5e6f34407ff60f
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-18 21:19:28 +0800

    validating patch method in object key value

--- functions/validate.js
+++ functions/validate.js
@@ -1,44 +1,67 @@
 function validate(body, validator, method = "PATCH", keyMessage = "") {
+    const error = {
+        isEmpty: () => false,
+        messages: [],
+        body: {}
+    }
     if (method === "PATCH") {
-        let newBody = {}
         const keys = Object.keys(body)
-        keys.map(key => {
-            const value = validator[key]
-            const typeValue = typeof value
-            if (typeValue === "object") {
-                newBody[key] = validate(value[key], value, method)
+        const length = keys.length
+        let i = 0
+        do {
+            const key = keys[i]
+            const keyMessages = `${keyMessage}${keyMessage ? "." : ""}${key}`
+            const validatorValue = validator[key]
+            const bodyValue = body[key]
+            const validatorTypeValue = typeof validatorValue
+            const bodyTypeValue = typeof bodyValue
+            if (validatorTypeValue === "object" ) {
+                if (validatorTypeValue !== bodyTypeValue) {
+                    error.messages.push(`${keyMessages} must be an object`)
+                    error.isEmpty = () => true
+                } else {
+                    const errorObject = validate(bodyValue, validatorValue, "POST", keyMessages)
+                    error.isEmpty = () => errorObject.isEmpty()
+                    error.messages.push(errorObject.messages)
+                    if (!error.isEmpty()) {
+                        error.body[key] = errorObject.body
+                    }
+                }
             }
-            else if (typeValue !== "undefined")
-                newBody[key] = body[key]
-        })
-        return newBody
+            else if (validatorTypeValue !== "undefined") {
+                error.body[key] = bodyValue
+            }
+            i++
+        } while (!error.isEmpty() && i < length)
+        // console.log(error)
+        return error
     } else if (method === "POST" || method === "PUT") {
-        const error = {
-            isEmpty: () => false,
-            messages: []
-        }
         const keys = Object.keys(validator)
         const length = keys.length
         let i = 0
         do {
             const key = keys[i]
             const keyMessages = `${keyMessage}${keyMessage ? "." : ""}${key}`
-            const value = body[key]
+            const bodyValue = body[key]
             const validatorValue = validator[key]
-            const typeValue = typeof value
+            const bodyTypeValue = typeof bodyValue
             const validatorTypeValue = typeof validatorValue
-            if (typeValue === validatorTypeValue) {
-                const errorObject = validate(value, validator[key], method, keyMessages)
-                error.isEmpty = () => errorObject.isEmpty()
-                error.messages.push(errorObject.messages)
+            if (validatorTypeValue === "object") {
+                if (bodyTypeValue !== validatorTypeValue) {
+                    error.messages.push(`${keyMessages} must be an object`)
+                    error.isEmpty = () => true
+                } else {
+                    const errorObject = validate(bodyValue, validatorValue, method, keyMessages)
+                    error.isEmpty = () => errorObject.isEmpty()
+                    error.messages.push(errorObject.messages)
+                }
             }
-            else if (typeValue === "undefined") {
+            else if (bodyTypeValue === "undefined") {
                 error.messages.push(`${keyMessages} is required`)
                 error.isEmpty = () => true
             }
-            else if (typeValue === "string" && validatorTypeValue === "object") {
-                error.messages.push(`${keyMessages} is object`)
-                error.isEmpty = () => true
+            else if (bodyTypeValue !== "undefined") {
+                error.body[key] = bodyValue
             }
             i++
         } while (!error.isEmpty() && i < length)
